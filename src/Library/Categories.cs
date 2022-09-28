8ea725fa1575c467cfdd1b5d99172820fa6c1f83
BravoToTo <francosanmartin@adinet.com.uy>
2022-09-28 19:50:37 -0300

    Added Populate Method

--- src/Library/Categories.cs
+++ src/Library/Categories.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Library;
@@ -5,6 +6,9 @@ namespace Library;
 public class Categories {
     private List<string> Category { get; set; }= new List<string>();
 
+    public Categories() {
+        Populate();
+    }
     protected void Add(Categories cat, string category){
         cat.Category.Add(category);
     }
@@ -14,5 +18,14 @@ public class Categories {
         {
             System.Console.WriteLine(item);
         }
+    }
+
+    public void Populate() {
+        String[] init = new String[]
+        {"Aerospace Engineer",
+        "Electrical Engineer",
+        "Chemical Engineer",
+        "Nuclear Engineer"};
+        this.Category.AddRange(init);
     }
 }
